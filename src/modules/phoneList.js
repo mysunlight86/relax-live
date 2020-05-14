6cb63b84942ae6ffab002bebdf69bcec389b0c6b
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-05-14 19:45:20 +0300

    Changed the phone list by switching classes

--- src/modules/phoneList.js
+++ src/modules/phoneList.js
@@ -1,18 +1,8 @@
 const phoneList = () => {
-  document.querySelector('.header-contacts__phone-number-accord').style.top = '0';
   document.querySelector('.header-contacts__arrow').addEventListener('click', event => {
-    // console.log(event.target.closest('.header-contacts__arrow'));
     const phoneNumAcc = document.querySelector('.header-contacts__phone-number-accord');
-    if (phoneNumAcc.style.top !== '25px') {
-      event.target.closest('.header-contacts__arrow').style.transform = `rotate(180deg)`;
-      phoneNumAcc.style.top = '25px';
-      phoneNumAcc.children[0].style.opacity = '1';
-      phoneNumAcc.style.width = '100%';
-    } else {
-      event.target.closest('.header-contacts__arrow').style.transform = `rotate(0deg)`;
-      phoneNumAcc.style.top = '0';
-      phoneNumAcc.children[0].style.opacity = '0';
-    }
+    phoneNumAcc.classList.toggle('header-accord-active');
+    event.target.closest('.header-contacts__arrow').classList.toggle('header-accord-arrow-active');
   });
 };
 

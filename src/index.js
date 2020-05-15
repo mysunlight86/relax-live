731e94d4ad75da2724003b1f62b3da90da332740
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-05-15 10:13:11 +0300

    when you click on the menu it leaves on the right

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import phoneList from './modules/phoneList';
+import showMenu from './modules/showMenu';
 
 phoneList();
+showMenu();
---
+++ src/modules/showMenu.js
@@ -0,0 +1,13 @@
+const showMenu = () => {
+  document.querySelector('.menu').addEventListener('click', () => {
+    const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+    const element = document.documentElement;
+    if (element.clientWidth > 1024) {
+      popupDialogMenu.style.right = `645px`;
+    } else if (element.clientWidth > 575) {
+      popupDialogMenu.style.right = `555px`;
+    }
+  });
+};
+
+export default showMenu;

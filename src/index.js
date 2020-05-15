fc7cd730cd2c62e22db11737461e55640e73c3ac
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-05-15 15:49:35 +0300

    Implemented smooth scrolling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import "@babel/polyfill";
+import elementClosest from 'element-closest';
+elementClosest(window);
+
+import smoothscroll from 'smoothscroll-polyfill';
+// kick off the polyfill!
+smoothscroll.polyfill();
+
 import phoneList from './modules/phoneList';
 import showMenu from './modules/showMenu';
 
--- src/modules/showMenu.js
+++ src/modules/showMenu.js
@@ -1,6 +1,8 @@
 const showMenu = () => {
+  const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+
+  // open menu
   document.querySelector('.menu').addEventListener('click', () => {
-    const popupDialogMenu = document.querySelector('.popup-dialog-menu');
     const element = document.documentElement;
     if (element.clientWidth > 1024) {
       popupDialogMenu.style.right = `645px`;
@@ -8,6 +10,28 @@ const showMenu = () => {
       popupDialogMenu.style.right = `555px`;
     } else {
       popupDialogMenu.style.top = `100vh`;
+    }
+  });
+
+  // smooth scroll
+  document.querySelector('body').addEventListener('click', event => {
+    const target = event.target;
+    try {
+      const blockID = target.getAttribute('href').substr(1);
+      if (
+        target.closest('a').tagName === 'A' &&
+        target.closest('a').getAttribute('href').indexOf('#') === 0
+      ) {
+        event.preventDefault();
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+        popupDialogMenu.style.right = `0`;
+        return;
+      }
+    } catch (error) {
+      return;
     }
   });
 };

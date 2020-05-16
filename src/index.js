0147ae3eb21207d266b36cedf766679772174f2a
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-05-16 15:15:43 +0300

    Implemented a mask for the fields by entering a phone number

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import smoothscroll from 'smoothscroll-polyfill';
 // kick off the polyfill!
 smoothscroll.polyfill();
 
+import maskPhone from './modules/maskPhone';
 import phoneList from './modules/phoneList';
 import showMenu from './modules/showMenu';
 import repairTypes from './modules/repairTypes';
 
+maskPhone('input[name="phone"]', '+7 (___) ___-__-__');
 phoneList();
 showMenu();
 repairTypes();
